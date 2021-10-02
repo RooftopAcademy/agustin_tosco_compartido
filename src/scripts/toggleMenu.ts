1da6d82572977a95bbf60514aebeb7f29ddbcfee
agustintosco <agustintosco.92@gmail.com>
2021-10-02 17:04:00 +0200

    reorg dir

--- src/scripts/toggleMenu.ts
+++
@@ -1,9 +0,0 @@
-export default function toggleMenu() : void {
-
-    let menu: HTMLElement = document.querySelector(".content")!;
-
-    let hidden: string = 'hidden';
-    let visible: string = 'visible';
-
-    menu.classList.contains(hidden)?menu.classList.replace(hidden, visible):menu.classList.replace(visible, hidden);
-}
