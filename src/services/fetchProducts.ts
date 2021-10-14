a122a181e44842d4c0f994ee0f4d9670a7a40ea3
agustintosco <agustintosco.92@gmail.com>
2021-10-14 21:31:51 +0200

    Added 'get-product' endpoint

--- src/services/fetchProducts.ts
+++ src/services/fetchProducts.ts
@@ -21,26 +21,5 @@ export default async function fetchProducts(store: Store) : Promise<void> {
                 store.catalog.add(product);
                 });
             console.log('Fetched from API');
-                
     });
-
-
-        // await fetch('https://61587a685167ba00174bbb19.mockapi.io/products')
-        //     .then((res) => (res.ok ? res.json() : Promise.reject(res)))
-        //     .then((products: Product[]) => {
-        //         products.forEach((item: Product) => {
-                
-        //         let product: Product = new Product;
-
-        //         product.id = item.id;
-        //         product.name = item.name;
-        //         product.price = item.price;
-        //         product.description = item.description;
-        //         product.stock = item.stock;
-        //         product.image = item.image;
-        //         product.category = item.category;
-
-        //         store.catalog.add(product);
-        //         });
-        // });
 }
