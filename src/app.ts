16fc8e67f14c06bdaac64d6aae8932b538de01ff
agustintosco <agustintosco.92@gmail.com>
2021-10-15 18:23:34 +0200

    Fixed routes and added endpoints

--- src/app.ts
+++ src/app.ts
@@ -37,42 +37,46 @@ useLocalStorage.set("store", store);
 
     await addListeners(store);
 
-    // let list = new Catalog;
+    /*
+     **  Trying sorting feature
+     */
 
-    // list.result = [...products];
+    let list = new Catalog;
 
-    // list.getSorting({'key': 'price', 'order': sortOrder.Asc})
+    list.result = [...products];
 
-    // const item = {
-    //     "name": "AmiBroker",
-    //     "price": 223,
-    //     "description": "",
-    //     "stock": 5,
-    //     "id": 8,
-    //     "image": "",
-    //     "category": 3
-    //   }
+    list.getSorting({'key': 'price', 'order': sortOrder.Desc})
 
-    // let product = new Product()
+    const item = {
+        "name": "AmiBroker",
+        "price": 223,
+        "description": "",
+        "stock": 5,
+        "id": 8,
+        "image": "",
+        "category": 3
+      }
 
-    // product.id = item.id;
-    // product.name = item.name;
-    // product.price = item.price;
-    // product.description = item.description;
-    // product.stock = item.stock;
-    // product.image = item.image;
-    // product.category = item.category;
+    let product = new Product()
+
+    product.id = item.id;
+    product.name = item.name;
+    product.price = item.price;
+    product.description = item.description;
+    product.stock = item.stock;
+    product.image = item.image;
+    product.category = item.category;
 
     //   list.result.push(product);
 
-    //   console.log(list.result);
+      //console.log(list.result);
       
-    // let key : keyof Product = "price";
-//   console.log(products.lastIndexOf(key));
+    let key : keyof Product = "price";
+    //console.log(products.lastIndexOf(key));
 
-    // let tempArray : string[] = [];
-    // tempArray = products.lastIndexOf('123'); 
+    let tempArray : string[] = [];
+    //tempArray = products.lastIndexOf('123'); 
 
-    // products.forEach((product) => console.log((product[key])));
+    //products.forEach((product) => console.log((product[key])));
 
 }());
--- src/backend/index.js
+++ src/backend/index.js
@@ -3,12 +3,14 @@ const app = express();
 const path = require('path');
 const dotenv = require('dotenv');
 const products = require('./api/routes/products');
+const categories = require('./api/routes/categories');
 
 dotenv.config();
 const port = process.env.port;
 
 app.use(express.static('public'));
 app.use('/products', products);
+// app.use('/categories', categories);
 
 app.locals.baseUrl = process.env.HOST + ':' + port;
 
@@ -24,11 +26,6 @@ app.get('/products-list', (req, res) => {
 
 app.get('/product-details', (req, res) => {
     let file = path.resolve("src", "views", "product-details.html")
-    res.sendFile(file);
-})
-
-app.get('/products/getAll', (req, res) => {
-    let file = path.resolve("src", "backend", "api", "products", "products.json")
     res.sendFile(file);
 })
 
